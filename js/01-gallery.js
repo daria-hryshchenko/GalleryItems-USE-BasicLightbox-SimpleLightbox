d8f9da96fc30cf9a837a6d3d0dd88ffa1a4d4817
Daria Hryshchenko <daria199210@gmail.com>
2022-12-28 10:56:03 +0200

    add solution tasks 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import {
+    galleryItems
+} from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkupEl = createGallery(galleryItems);
+
+galleryEl.innerHTML = galleryMarkupEl;
+
+
+//create elements for gallery
+
+function createGallery(images) {
+    return images.map(({
+            preview,
+            original,
+            description
+        }) =>
+        `<a href=${original} class="gallery__link">
+        <img src=${preview} data-source="large-image.jpg" class="gallery__image" alt=${description}/> 
+        </a>`
+    ).join("");
+};
+
+// check to click a  image  
+galleryEl.addEventListener('click', modalImg);
+
+function modalImg(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(
+        `<img src="${event.target.closest('.gallery__link').href}"
+        width="800"
+        height="600">`
+    );
+    instance.show();
+
+    window.addEventListener("keydown", (event) => {
+        if (event.code === 'Escape') {
+            instance.close();
+        }
+    })
+
+}
